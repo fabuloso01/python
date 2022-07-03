863abef6a03acd700fdf272e7f6e4c6f5c27f93b
fabuloso01 <fabuloso01@github.com>
2022-07-03 14:51:36 -0400

    Third commit

--- ER.py
+++ ER.py
@@ -69,7 +69,7 @@ for valor in range(1,11):
 '''
 '''
 x=11
-for a in range(1,4):
+for a in range(0,4):
     for x in range(1, 11):
         print(x, end=" ")
 '''
@@ -87,10 +87,103 @@ while True:
         print('Fim do programa.')
         break
 '''
- '''
+'''
 for i in range(1,11):
     n = int(input("Informe um número de 0-10: "))
     if n < 0 or n > 10:
         print('Fim do programa.')
         break
 '''
+'''
+lista_produtos = ["faca", "garfo", "panela", "frigideira"]
+
+for produto in lista_produtos:
+    print(produto.capitalize())
+'''
+"""
+lista_precos = [10, 20, 30]
+for preco in lista_precos:
+    imposto = preco*0.1
+    print(f"{imposto:.2f}")
+"""
+
+'''
+#percorrer um dicionário
+produtos = {
+    "faca": 10,
+    "garfo": 20,
+}
+
+for produto in produtos:
+    print(produto)
+    print(produtos[produto], end=" ")
+'''
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  if x == "banana":
+    break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+'''
+'''
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+  for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+'''
+'''
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+'''
+'''
+y = 6
+for x in range(0,5):
+    for y in range(0,6):
+        print(y,end=" ")
+else:
+    print("Fim")
+'''
+
+n = 0
+somar = 0
+num_digitados = 0
+for i in range(1,11):
+    n = int(input("Informe um número de 0-10: "))
+    if n < 0 or n > 10:
+        print(f'Soma dos números digitados: {somar}')
+        print(f'Quantidade de números digitados: {num_digitados}')
+        print('Fim do programa.')
+        break
+    somar += n 
+    print(somar)
+    num_digitados += 1
+
+    
+
+
+
+
+
+
